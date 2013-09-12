ac5ca029425de7bc97df4ef167b84defa1596fec
Matthew Lew <mlew@stanford.edu>
2013-09-12 15:46:53 -0700

    Fixed bug in f_concatSMfits - restores support of projects that have no fiduciary corrections

--- f_concatSMfits.m
+++ f_concatSMfits.m
@@ -32,53 +32,54 @@ function [totalPSFfits, numFrames, fidTrackX, fidTrackY, fidTrackZ] = ...
 numSyncFrames = 25;
 useDenoising = 1;
 
-%% load raw fiduciary data
-% [fidFile fidPath] = uigetfile({'*.mat';'*.*'},'Open data file #1 with raw fiduciary fits');
-for fileNum=1:length(fidFilePrefix)
-    %         fidFiles = [fidFiles; {[fidPath fidFile]}];
-    % load data
-    load([fidFilePrefix{fileNum} 'raw fits.mat'],'PSFfits','numFrames','numMoles');
-    
-    if fileNum == 1
-        tempPSFfits = PSFfits(:,1:23);
-        numFramesInFiles = numFrames;
-    else
-        numFramesInFiles = [numFramesInFiles numFrames];
-        PSFfits(:,1) = PSFfits(:,1) + sum(numFramesInFiles(1:fileNum-1));
-        tempPSFfits = [tempPSFfits; PSFfits(:,1:23)];
+if useFidCorrections
+
+    %% load raw fiduciary data
+    for fileNum=1:length(fidFilePrefix)
+        %         fidFiles = [fidFiles; {[fidPath fidFile]}];
+        % load data
+        load([fidFilePrefix{fileNum} 'raw fits.mat'],'PSFfits','numFrames','numMoles');
+
+        if fileNum == 1
+            tempPSFfits = PSFfits(:,1:23);
+            numFramesInFiles = numFrames;
+        else
+            numFramesInFiles = [numFramesInFiles numFrames];
+            PSFfits(:,1) = PSFfits(:,1) + sum(numFramesInFiles(1:fileNum-1));
+            tempPSFfits = [tempPSFfits; PSFfits(:,1:23)];
+        end
+
+        %         fileNum = fileNum+1;
+        %         [fidFile fidPath] = uigetfile({'*.mat';'*.*'},...
+        %             ['Open data file #' num2str(fileNum) ' with raw fiduciary fits']);
     end
-    
-    %         fileNum = fileNum+1;
-    %         [fidFile fidPath] = uigetfile({'*.mat';'*.*'},...
-    %             ['Open data file #' num2str(fileNum) ' with raw fiduciary fits']);
-end
-PSFfits = tempPSFfits;
-numFrames = sum(numFramesInFiles);
-clear tempPSFfits;
+    PSFfits = tempPSFfits;
+    numFrames = sum(numFramesInFiles);
+    clear tempPSFfits;
 
-fidTrackX = NaN(size(PSFfits,1),numMoles);
-fidTrackY = NaN(size(PSFfits,1),numMoles);
-fidTrackZ = NaN(size(PSFfits,1),numMoles);
+    fidTrackX = NaN(size(PSFfits,1),numMoles);
+    fidTrackY = NaN(size(PSFfits,1),numMoles);
+    fidTrackZ = NaN(size(PSFfits,1),numMoles);
 
-for molecule = 1:numMoles
-    % extract fitting parameters for this molecule
-    moleculeFitParam = PSFfits(PSFfits(:,2) == molecule, :);
-    
-    goodFitFlag(:,molecule) = moleculeFitParam(:,13);
-    goodFit = goodFitFlag(:,molecule) > 0;
-    
-    % raw positions of the fiducial tracks
-    fidTrackX(goodFit,molecule) = moleculeFitParam(goodFit,21);
-    fidTrackY(goodFit,molecule) = moleculeFitParam(goodFit,22);
-    fidTrackZ(goodFit,molecule) = moleculeFitParam(goodFit,23);
-    %     numPhotons(:,molecule) = moleculeFitParam(:,17);
-    
-    
-end
+
+    for molecule = 1:numMoles
+        % extract fitting parameters for this molecule
+        moleculeFitParam = PSFfits(PSFfits(:,2) == molecule, :);
+
+        goodFitFlag(:,molecule) = moleculeFitParam(:,13);
+        goodFit = goodFitFlag(:,molecule) > 0;
+
+        % raw positions of the fiducial tracks
+        fidTrackX(goodFit,molecule) = moleculeFitParam(goodFit,21);
+        fidTrackY(goodFit,molecule) = moleculeFitParam(goodFit,22);
+        fidTrackZ(goodFit,molecule) = moleculeFitParam(goodFit,23);
+        %     numPhotons(:,molecule) = moleculeFitParam(:,17);
+
+
+    end
 
 
 %%
-if useFidCorrections
     % compute movement of fiduciaries
     devX = zeros(numFrames,numMoles);
     devY = zeros(numFrames,numMoles);
@@ -207,12 +208,7 @@ if exist('tempAvgDevX','var')
     %         'concatenation info');
     
 else
-    
-    % load localization data
-    %     [moleFile molePath] = uigetfile({'*.mat';'*.*'},'Open data file #1 with PSF localizations');
-    
-    %     moleFiles = {};
-    
+    % just concatenate PSF fits together without compensating for drift
     for fileNum=1:length(fitFilePrefix)
         %         moleFiles = [moleFiles; {[molePath moleFile]}];
         clear numFrames;
@@ -244,8 +240,9 @@ else
     %     numFrames = sum(numFramesInFiles);
     clear tempPSFfits;
     
-    
-    
+    fidTrackX = [];
+    fidTrackY = [];
+    fidTrackZ = [];
     
     % output concatenated data
     %     save([saveFilePrefix 'molecule fits.mat']);
